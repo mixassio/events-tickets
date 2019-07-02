d01008eaebc239f6ccf2e226ade722e4989ec345
mixassio <mixail.bondarev@gmail.com>
2019-07-02 21:10:29 +0300

    first commit

---
+++ src/user/user.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column('email')
+  email: string;
+
+}
